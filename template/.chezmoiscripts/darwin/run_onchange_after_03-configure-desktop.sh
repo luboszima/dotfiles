577a419e76df15f37331c62dc66526901f573b77
Lubos Zima <lubos.zima@gmail.com>
2024-05-24 23:44:30 +0700

    update scripts

--- template/.chezmoiscripts/darwin/run_onchange_after_03-configure-desktop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eufo pipefail
-
-file="/System/Library/Desktop Pictures/Big Sur Waters Edge.madesktop"
-osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"${file}\" as POSIX file"
