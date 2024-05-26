d66d3b012960af79de8389e156c4b32ae2da7ca3
Lubos Zima <lubos.zima@gmail.com>
2024-05-26 15:40:26 +0700

    skip install oh-my-zsh

--- template/.chezmoiscripts/darwin/run_once_after_02-configure-oh-my-zsh.sh
+++ template/.chezmoiscripts/darwin/run_once_after_02-configure-oh-my-zsh.sh
@@ -2,4 +2,8 @@
 
 set -eufo pipefail
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if ! [ -d "~/.oh-my-zsh/" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+  echo "Skipping .oh-my-zsh, already installed."
+fi
