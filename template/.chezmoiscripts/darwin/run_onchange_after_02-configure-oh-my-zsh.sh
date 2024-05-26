9fd18f8bcd133fb3c4a574620b4376ebd0427126
Lubos Zima <lubos.zima@gmail.com>
2024-05-26 18:31:58 +0700

    test

--- template/.chezmoiscripts/darwin/run_onchange_after_02-configure-oh-my-zsh.sh
+++ template/.chezmoiscripts/darwin/run_onchange_after_02-configure-oh-my-zsh.sh
@@ -2,8 +2,8 @@
 
 set -eufo pipefail
 
-if ! [ -d "~/.oh-my-zsh/" ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if ! [ -d "~/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || true
 else
   echo "Skipping .oh-my-zsh, already installed."
 fi
