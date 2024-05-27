1a9533f21c454688598d33c23bfdb57983aa3099
Lubos Zima <lubos.zima@gmail.com>
2024-05-28 00:04:38 +0700

    add: inline params for install script

--- install.sh
+++ install.sh
@@ -2,6 +2,32 @@
 
 set -e # -e: exit on error
 
+function usage() {
+  echo "Usage: $0 <email> <first-name> <family-name> <github-username>"
+}
+
+# help if -h param
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    usage
+    exit 0;
+fi
+
+if ! [ -z "$1" ]; then
+  CHZ_EMAIL="$1"
+fi
+
+if ! [ -z "$2" ]; then
+  CHZ_FIRSTNAME="$2"
+fi
+
+if ! [ -z "$3" ]; then
+  CHZ_FAMILYNAME="$3"
+fi
+
+if ! [ -z "$4" ]; then
+  CHZ_GITHUBUSERNAME="$4"
+fi
+
 prompt=""
 if ! [ -z "$CHZ_EMAIL" ]; then
   prompt="$prompt --promptString email=$CHZ_EMAIL"
